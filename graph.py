5f5617dca729c514c8f9a8e2146b935e10c00512
Michael Crouse <46467432+mjcrouse@users.noreply.github.com>
2020-09-15 11:55:40 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,10 +1,12 @@
+import random
+
 graph = {}
 
 def getNumNodes():
     while True:
         try:
             numNodes = int(input("How many nodes? "))
-            while numNodes > 26:
+            while numNodes > 30:
                 print("Too many nodes!")
                 numNodes = getNumNodes()
             while numNodes < 0:
@@ -18,4 +20,25 @@ def getNumNodes():
 
 
 numNodes = getNumNodes()
+
+def setupRandomGraph(numNodes):
+    for i in range(1,numNodes+1):
+        graph[i] = []
+        if i < numNodes - 3:
+            for j in range(random.randint(2,5)):
+                addme = i + random.randint(1,3)
+                if addme not in graph[i]:
+                    graph[i].append(addme)
+        if i > numNodes - 4 and i < numNodes - 2:
+            for j in range(random.randint(0,3)):
+                addme = i + random.randint(1,3)
+                if addme not in graph[i]:
+                    graph[i].append(addme)
+    
+            
+
+setupRandomGraph(numNodes)
+print(graph)
+
+
 
